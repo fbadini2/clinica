654b2edbf75371e485a1d1031a1b1080800554c2
Fabian <fbadini@gmail.com>
2021-07-13 23:42:59 -0300

    conexion a mysql

--- src/App.py
+++ src/App.py
@@ -3,6 +3,13 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 sqlconn = MySQL()
 
+# conexion a mysql
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '#171Nolaadi'
+app.config['MYSQL_DB'] = 'flaskcontacts'
+mysql = MySQL(app)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
